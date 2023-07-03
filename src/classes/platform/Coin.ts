f0d2b81cac35ef3af7d2cb4d8dfc230b4a839231
smartkiiwii <nhan.nguyen1720dk@hcmut.edu.vn>
2023-07-03 10:23:14 +0700

    adjust coin size

--- src/classes/platform/Coin.ts
+++ src/classes/platform/Coin.ts
@@ -1,10 +1,17 @@
 import Player from "../player/Player"
 import { SetPlayerDataAction, getPlayerData, setPlayerData } from "../player/PlayerContext"
+import { playCoinPickup } from "../sound-manager/SoundManager"
 import Platform from "./Platform"
 import PlatformCondiment from "./PlatformCondiment"
 
 export default class Coin extends PlatformCondiment {
     private platform: Platform | undefined
+
+    constructor(scene: Phaser.Scene, x: number, y: number, texture: string | Phaser.Textures.Texture, frame?: string | number) {
+        super(scene, x, y, texture, frame)
+
+        this.setScale(2)
+    }
 
     public attachToPlatform(platform: Platform): void {
         this.platform = platform
@@ -18,6 +25,9 @@ export default class Coin extends PlatformCondiment {
     
     public onCollisionWithPlayer(player: Player, isAccurateHit: boolean, _isLeftSide: boolean): void {
         if (isAccurateHit) {
+            // play audio
+            playCoinPickup(this.scene)
+            
             setPlayerData(this.scene, {
                 type: SetPlayerDataAction.SET_COINS,
                 payload: getPlayerData(this.scene).coins + 1,
